abbceca2fae615816fde4fff13450f2c9d2c9e85
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2023-08-10 11:45:53 -0500

    Add calculate how many days until next birthday method

--- BirthdayApplication/Program.cs
+++ BirthdayApplication/Program.cs
@@ -10,10 +10,10 @@ namespace BirthdayApplication
             Console.WriteLine("Enter your birthday in M/DD/YY format");
             string birthdayString = Console.ReadLine();
             DateTime birthday = StringToDateConverter(birthdayString);
-            HowManyDaysOld(birthday);
+            HowManyYearsOld(birthday);
             HowManyMonthsOld(birthday);
             HowManyDaysOld(birthday);
-
+            CalculateNextBirthdayDays(birthday);
             Console.ReadLine();
         }
 
@@ -70,6 +70,26 @@ namespace BirthdayApplication
             TimeSpan difference = now - birthday; 
             int days = difference.Days;
             Console.WriteLine($"You are {days} days old.");
+        }
+
+        private static void CalculateNextBirthdayDays(DateTime birthday)
+        {
+            DateTime now = DateTime.Now;
+            // e.g., 2023, January, 2
+            DateTime nextBirthday = new DateTime(now.Year, birthday.Month, birthday.Day);
+
+            // If the birthday has already occurred this year, add one year to get the next
+            // if 2023, January, 2 < 2023, August, 10, add a year so 2024 is next birthday year
+            if (nextBirthday < now)
+            {
+                nextBirthday = nextBirthday.AddYears(1);
+            }
+
+            // Subtract the current date from the next birthday date
+            // e.g., difference = 2024, January, 2 - 2023, August, 10
+            TimeSpan difference = nextBirthday - now;
+            int days = difference.Days;
+            Console.WriteLine($"It will be {days} days until your next birthday!");
         }
 
     }
